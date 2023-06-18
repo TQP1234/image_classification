4d31d79f0bf0807b2cf23d431b7fb43cff9a1053
TQP1234 <tqp1234@gmail.com>
2023-06-18 13:05:04 +0800

    Update model architecture

--- model.py
+++ model.py
@@ -14,20 +14,62 @@ class SimpleConvNet(nn.Module):
         self.ConvBlock1 = nn.Sequential(
             OrderedDict([
                 ('conv1', nn.Conv2d(input_channel, 32, 3, 1, 1)),
+                ('batchnorm1', nn.BatchNorm2d(32)),
                 ('leaky1', nn.LeakyReLU(inplace=True)),
                 ('pool1', nn.MaxPool2d(2, 2)),
                 ('conv2', nn.Conv2d(32, 64, 3, 1, 1)),
-                ('leaky2', nn.LeakyReLU(inplace=True)),
-                ('pool2', nn.MaxPool2d(2, 2)),
-                ('conv3', nn.Conv2d(64, 128, 3, 1, 1)),
-                ('leaky3', nn.LeakyReLU(inplace=True)),
-                ('pool3', nn.MaxPool2d(2, 2)),
-                ('conv4', nn.Conv2d(128, 256, 3, 1, 1)),
-                ('leaky4', nn.LeakyReLU(inplace=True))
+                ('batchnorm2', nn.BatchNorm2d(64)),
+                ('leaky2', nn.LeakyReLU(inplace=True))
             ])
         )
 
         self.SqueezeAndExcitation1 = nn.Sequential(
+            OrderedDict([
+                ('globalpool1', nn.AvgPool2d(input_size//2, 1)),  # global average pooling
+                ('flat1', nn.Flatten()),  # convert the dimension from (B, C, 1, 1) to (B, C)
+                ('fc1', nn.Linear(64, 64//r)),
+                ('relu1', nn.ReLU(inplace=True)),
+                ('fc2', nn.Linear(64//r, 64)),
+                ('sigmoid1', nn.Sigmoid())
+            ])
+        )
+
+        self.ConvBlock2 = nn.Sequential(
+            OrderedDict([
+                ('conv1', nn.Conv2d(64, 64, 3, 1, 1)),
+                ('batchnorm1', nn.BatchNorm2d(64)),
+                ('leaky1', nn.LeakyReLU(inplace=True)),
+                ('pool1', nn.MaxPool2d(2, 2)),
+                ('conv2', nn.Conv2d(64, 128, 3, 1, 1)),
+                ('batchnorm2', nn.BatchNorm2d(128)),
+                ('leaky2', nn.LeakyReLU(inplace=True))
+            ])
+        )
+
+        self.SqueezeAndExcitation2 = nn.Sequential(
+            OrderedDict([
+                ('globalpool1', nn.AvgPool2d(input_size//4, 1)),  # global average pooling
+                ('flat1', nn.Flatten()),  # convert the dimension from (B, C, 1, 1) to (B, C)
+                ('fc1', nn.Linear(128, 128//r)),
+                ('relu1', nn.ReLU(inplace=True)),
+                ('fc2', nn.Linear(128//r, 128)),
+                ('sigmoid1', nn.Sigmoid())
+            ])
+        )
+
+        self.ConvBlock3 = nn.Sequential(
+            OrderedDict([
+                ('conv1', nn.Conv2d(128, 128, 3, 1, 1)),
+                ('batchnorm1', nn.BatchNorm2d(128)),
+                ('leaky1', nn.LeakyReLU(inplace=True)),
+                ('pool1', nn.MaxPool2d(2, 2)),
+                ('conv2', nn.Conv2d(128, 256, 3, 1, 1)),
+                ('batchnorm2', nn.BatchNorm2d(256)),
+                ('leaky2', nn.LeakyReLU(inplace=True))
+            ])
+        )
+
+        self.SqueezeAndExcitation3 = nn.Sequential(
             OrderedDict([
                 ('globalpool1', nn.AvgPool2d(input_size//8, 1)),  # global average pooling
                 ('flat1', nn.Flatten()),  # convert the dimension from (B, C, 1, 1) to (B, C)
@@ -38,31 +80,10 @@ class SimpleConvNet(nn.Module):
             ])
         )
 
-        self.ConvBlock2 = nn.Sequential(
+        self.ConvBlock4 = nn.Sequential(
             OrderedDict([
-                ('conv1', nn.Conv2d(256, 512, 3, 1, 1)),
-                ('leaky1', nn.LeakyReLU(inplace=True)),
-                ('pool1', nn.MaxPool2d(2, 2)),
-                ('conv2', nn.Conv2d(512, 1024, 3, 1, 1)),
-                ('leaky2', nn.LeakyReLU(inplace=True)),
-                ('pool2', nn.MaxPool2d(2, 2))
-            ])
-        )
-
-        self.SqueezeAndExcitation2 = nn.Sequential(
-            OrderedDict([
-                ('globalpool1', nn.AvgPool2d(input_size//32, 1)),  # global average pooling
-                ('flat1', nn.Flatten()),  # convert the dimension from (B, C, 1, 1) to (B, C)
-                ('fc1', nn.Linear(1024, 1024//r)),
-                ('relu1', nn.ReLU(inplace=True)),
-                ('fc2', nn.Linear(1024//r, 1024)),
-                ('sigmoid1', nn.Sigmoid())
-            ])
-        )
-
-        self.ConvBlock3 = nn.Sequential(
-            OrderedDict([
-                ('conv1', nn.Conv2d(1024, 1024, 3, 1, 1)),
+                ('conv1', nn.Conv2d(256, 1024, 1, 1)),
+                ('batchnorm1', nn.BatchNorm2d(1024)),
                 ('leaky1', nn.LeakyReLU(inplace=True))
             ])
         )
@@ -74,7 +95,7 @@ class SimpleConvNet(nn.Module):
         # Found that it achieved higher accuracy than just pure flattening
         self.AverageGlobalPooling = nn.Sequential(
             OrderedDict([
-                ('globalpool1', nn.AvgPool2d(input_size//32, 1)),  # global average pooling
+                ('globalpool1', nn.AvgPool2d(input_size//8, 1)),  # global average pooling
                 ('flat1', nn.Flatten())
             ])
         )
@@ -94,7 +115,7 @@ class SimpleConvNet(nn.Module):
         X = self.ConvBlock1(X)
 
         # Squeeze and Excitation Layer 1
-        S = self.SqueezeAndExcitation1(X).view(-1, 256, 1, 1)
+        S = self.SqueezeAndExcitation1(X).view(-1, 64, 1, 1)
         # Scaling
         X = X * S.expand_as(X)
 
@@ -102,12 +123,20 @@ class SimpleConvNet(nn.Module):
         X = self.ConvBlock2(X)
 
         # Squeeze and Excitation Layer 2
-        S = self.SqueezeAndExcitation2(X).view(-1, 1024, 1, 1)
+        S = self.SqueezeAndExcitation2(X).view(-1, 128, 1, 1)
         # Scaling
         X = X * S.expand_as(X)
 
         # Conv block 3
         X = self.ConvBlock3(X)
+
+        # Squeeze and Excitation Layer 3
+        S = self.SqueezeAndExcitation3(X).view(-1, 256, 1, 1)
+        # Scaling
+        X = X * S.expand_as(X)
+
+        # Conv block 4
+        X = self.ConvBlock4(X)
 
         # Dense layer
         X = self.AverageGlobalPooling(X)
